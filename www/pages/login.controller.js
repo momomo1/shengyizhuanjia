bb94190c7431a5b677a16dece008ef7f700c7f3f
momomo1 <32104300+momomo1@users.noreply.github.com>
2017-09-28 01:31:25 +0800

    zyt
    等等

---
+++ www/pages/login.controller.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/9/18.
+ */
+(function () {
+  'use strict';
+  //var USER_KEY = 'User';
+  angular.module('starter.controllers')
+    .controller('LoginCtrl',['$scope'])
+
+})
